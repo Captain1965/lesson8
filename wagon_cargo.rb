8e820bd2045d0cbb30cb180eea7f559ea6fc2f0b
Captain1965 <alex1965-03@mail.ru>
2021-12-24 05:33:35 +0300

    выполнено домашнее задание для классов WagonCargo , WagonPassenger

--- wagon_cargo.rb
+++ wagon_cargo.rb
@@ -1,12 +1,32 @@
 #encoding utf-8
 #Добавляется определение типа вагона type cargo
+# Добавить атрибут общего объема (задается при создании вагона)
+# Добавить метод, которые "занимает объем" в вагоне (объем указывается в качестве параметра метода)
+# Добавить метод, который возвращает занятый объем
+# Добавить метод, который возвращает оставшийся (доступный) объем
+
 require_relative 'wagon'
-require_relative 'train'
-require_relative 'wagon'
-require_relative 'wagon_passenger'
+
 class WagonCargo < Wagon
-	attr_reader :type
-	def initialize
+	attr_accessor :number
+	attr_reader :type, :volume
+
+	def initialize(volume)
+		@volume = volume
+		@volume_busy  = 0
 		@type = 'cargo'
+	end
+
+	def load_wagon(volume_cargo)
+		@volume_busy += volume_cargo if volume_cargo <= self.volume_free
+
+	end
+
+	def volume_busy
+		@volume_busy
+	end
+
+	def volume_free
+	 @volume - @volume_busy
 	end
 end
--- wagon_passenger.rb
+++ wagon_passenger.rb
@@ -1,11 +1,31 @@
 #encoding utf-8
 #Добавляется определение типа вагона type passeger
+# Для пассажирских вагонов:
+# Добавить атрибут общего кол-ва мест (задается при создании вагона)
+# Добавить метод, который "занимает места" в вагоне (по одному за раз)
+# Добавить метод, который возвращает кол-во занятых мест в вагоне
+# Добавить метод, возвращающий кол-во свободных мест в вагоне.
 
 require_relative 'wagon'
 class WagonPassenger < Wagon
 	attr_accessor :number
-	attr_reader :type
-	def initialize
+	attr_reader :type, :seats
+	def initialize(seats)
 		@type = 'passenger'
+		@seat = 0
+		@seats = seats
+	end
+
+	def seat
+		@seat +=1 if @seat < @seats
+	end
+
+	def seat_busy
+		@seat
+	end
+
+	def seat_free
+		seat_free = @seats - @seat
 	end
 end
+
